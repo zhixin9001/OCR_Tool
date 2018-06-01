6471cfcade2a4e57758c2b3f9c351c6d954a824b
dz33 <zhixin9001@live.com>
2018-06-01 12:42:59 +0800

    base64

---
+++ OCR_Tool/Models.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCR_Tool
+{
+    public class Result
+    {
+        public string words;
+    }
+
+    public class Auth
+    {
+        public string API_KEY;
+        public string SECRET_KEY;
+    }
+}
